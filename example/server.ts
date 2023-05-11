5e58f741d4c988f33d0598de99e838e0cc074b10
Alisue <lambdalisue@gmail.com>
2023-05-11 20:53:47 +0900

    Update examples

--- example/server.ts
+++ example/server.ts
@@ -1,6 +1,5 @@
-import { deferred } from "https://deno.land/std@0.186.0/async/mod.ts";
 import * as unknownutil from "https://deno.land/x/unknownutil@v2.1.1/mod.ts";
-import { Session } from "../mod.ts";
+import { Client, Session } from "../mod.ts";
 
 async function main(): Promise<void> {
   const hostname = "localhost";
@@ -16,31 +15,30 @@ async function main(): Promise<void> {
 }
 
 async function startSession(conn: Deno.Conn): Promise<void> {
-  const guard = deferred();
   const session = new Session(conn.readable, conn.writable);
-  await session.start(async (client) => {
-    session.dispatcher = {
-      sum(x, y) {
-        unknownutil.assertNumber(x);
-        unknownutil.assertNumber(y);
-        return x + y;
-      },
+  const client = new Client(session);
+  session.dispatcher = {
+    sum(x, y) {
+      unknownutil.assertNumber(x);
+      unknownutil.assertNumber(y);
+      return x + y;
+    },
 
-      helloServer(name) {
-        unknownutil.assertString(name);
-        return `Hello ${name}, this is server`;
-      },
+    helloServer(name) {
+      unknownutil.assertString(name);
+      return `Hello ${name}, this is server`;
+    },
 
-      helloClient(name) {
-        return client.request("helloClient", name);
-      },
+    helloClient(name) {
+      return client.call("helloClient", name);
+    },
 
-      helloClientServer(name) {
-        return client.request("helloServer", name);
-      },
-    };
-    await guard;
-  });
+    helloClientServer(name) {
+      return client.call("helloServer", name);
+    },
+  };
+  session.start();
+  await session.wait();
 }
 
 main();
